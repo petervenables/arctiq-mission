a6e5489f50a26cb986f3d8d318f909a8a9d5eaed
Peter Venables <pvenables@gmail.com>
2023-03-12 13:54:28 -0400

    store state in gcp pt1

--- microservices-demo/terraform/main.tf
+++ microservices-demo/terraform/main.tf
@@ -91,3 +91,18 @@ resource "null_resource" "wait_conditions" {
     resource.null_resource.apply_deployment
   ]
 }
+
+# Set up Google Cloud Storage for Terraform State usage
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
